3685a7076a2d5a5330edf0a4b3d02646bc720acc
Dwight Lindquist <dwight.lindquist@gmail.com>
2022-04-21 14:35:02 -0400

    updated with all tests passed

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,44 +1,73 @@
 import re 
 
-print('Welcome to our madlib cli. We will take your text input from the command line, then put each word of input into our madlib to make a complete text.')
-
 #function1 that takes dsn.txt and takes out the {adj} {noun} etc., store in a list to be used later 
 
 def read_template(filepath):
-  with open("./assets/dark_and_stormy_night_template.txt", "r") as f:  
+  # with open("./assets/dark_and_stormy_night_template.txt", "r") as f:  
+  with open(filepath, "r") as f:
     stripped_str = f.read().strip()
     return stripped_str
 
+def parse_template(template): 
+  parts = []
+  stripped = ""
+  capturing = False 
+  capture = ""
+  for char in template: 
+    if not capturing: 
+      stripped += char 
+      if char == "{":
+        capturing = True
+    else: 
+      if char == "}": 
+        stripped += char 
+        parts.append(capture)
+        capture = ""
+        capturing = False
+      else: 
+        capture += char
+  #this return value will be a nested tuple 
+  return stripped, tuple(parts) 
 
-def parse_template(my_str):
+# def parse_template(template):
+#   #expects: ("It was a {} and {} {}.", ("Adjective", "Adjective", "Noun"))
+#   # this will default to returning a tuple: return "It was a {} and {} {}.", ("Adjective", "Adjective", "Noun")
+#   # with open("./assets/dark_and_stormy_night_template.txt", "r") as f:
+#     # print(f, 'f')
 
-  # with open("./assets/dark_and_stormy_night_template.txt", "r") as f:
-    # print(f, 'f')
-
-    regex = '{[^}]*}'
-    parts_with_curlies = str(re.findall(regex,my_str))
-    print('parts with curlies: ', parts_with_curlies)
-    #get words from pwc as a tuple and return that 
-    stripped = re.sub(regex, '{}', my_str)
-    print('stripped: ', stripped)
-    return stripped
+#     regex = '{[^}]*}'
+#     parts_with_curlies = str(re.findall(regex, template))
+#     print('parts with curlies: ', parts_with_curlies)
+#     #get words from pwc as a tuple and return that 
+#     stripped = re.sub(regex, '{}', template)
+#     print('stripped: ', stripped)
+#     return stripped
 
 # print(parse_template("./assets/dark_and_stormy_night_template.txt"))
 
-# terms = regex.findall(r'/{[^}]*}/g')
-# cleaned_str = regex.sub(terms, '{}', input_str)
 
-#function2 >> give user a series of inputs (number of inputs can vary based on number of spaces to fill)
-#function3 >> put inputted words into madlib text
+def merge(stripped_template, parts):
+  # *parts unpacks the tuple 
+  return stripped_template.format(*parts)
 
-#final text: put in commandline 
-#final text: put in a new .txt file
+def collect_input(parts): 
+  result = []
+  for part in parts: 
+    response = input(f'enter a {part} ')
+    result.append(response)
+  return result
 
-
-def merge():
-  pass
-
-
+if __name__ == "__main__": 
+  print('Welcome to our madlib cli. We will take your text input from the command line, then put each word of input into our madlib to make a complete text.')
+  path = "assets/dark_and_stormy_night_template.txt"
+  template = read_template(path)
+  stripped, parts = parse_template(template)
+  responses = collect_input(parts) 
+  merged = merge(stripped, responses)
+  print(merged)
+  out_path = path.replace(".txt", "_completed.txt")
+  with open(out_path, "w") as f: 
+    f.write(merged)
   
   
 
